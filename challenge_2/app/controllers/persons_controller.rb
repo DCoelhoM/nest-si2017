6784404cd835c504cdd4d5187d3de53769776f90
Dinis Coelho Marques <dinis.c.mrqs@gmail.com>
2017-06-01 15:40:33 +0100

    show person info

--- challenge_2/app/controllers/persons_controller.rb
+++ challenge_2/app/controllers/persons_controller.rb
@@ -7,16 +7,12 @@ class PersonsController < ApplicationController
     @person = Person.find(params[:id])
   end
 
-  def new
-    @person = Person.new
-  end
-
   def create
-    #@person = Person.new()//TODO
-    #if @person.save
-    #  redirect to @person
-    #else
-    #  render 'index'
-    #end
+    @person = Person.new(params.require(:person).permit(:name, :company, :email, :phone, :photo))
+    if @person.save
+      redirect_to @person
+    else
+      render 'index'
+    end
   end
 end
