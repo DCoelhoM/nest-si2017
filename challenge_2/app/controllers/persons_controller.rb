8ca7f50cf50c228df492b6153b158f38e2b491da
Dinis Coelho Marques <dinis.c.mrqs@gmail.com>
2017-06-01 14:46:12 +0100

    list all persons

---
+++ challenge_2/app/controllers/persons_controller.rb
@@ -0,0 +1,22 @@
+class PersonsController < ApplicationController
+  def index
+    @persons = Person.all
+  end
+
+  def show
+    @person = Person.find(params[:id])
+  end
+
+  def new
+    @person = Person.new
+  end
+
+  def create
+    #@person = Person.new()//TODO
+    #if @person.save
+    #  redirect to @person
+    #else
+    #  render 'index'
+    #end
+  end
+end
